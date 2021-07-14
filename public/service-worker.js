6d6a2bdd387ba5c93e9d88bdde961f649dae9b6f
Brian McMullen <clanmcmullen2012@gmail.com>
2021-07-14 18:52:55 -0400

    redeploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const FILES_TO_CACHE = [
     "/styles.css",
     "/db.js",
     "/icons/icon-192x192.png",
-    "/icons/icon-512x512/png"
+    "/icons/icon-512x512.png"
 ];
 
 const PRECACHE = 'precache-v1';
